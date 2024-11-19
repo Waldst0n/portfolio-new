d8500a621bba27cd87063d0aca962e70f6b48629
Waldston <waldston.ws@gmail.com>
2024-11-19 19:26:09 -0300

    feat: Add Main Container Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
+import MainContainer from '@/components/main-container';
 import Navbar from '@/components/navbar';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <header className="flex w-full justify-center items-center">
+      <div className="flex m-auto  w-[1260px] flex-col  mb-16 justify-center items-center">
         <Navbar />
-      </header>
+        <MainContainer />
+      </div>
     </>
   );
 }
---
+++ src/components/main-container.js
@@ -0,0 +1,11 @@
+export default function MainContainer() {
+  return (
+    <>
+      <main className=" flex items-center justify-start px-16  w-full">
+        <div>
+          <h1>Sou o container principal</h1>
+        </div>
+      </main>
+    </>
+  );
+}
