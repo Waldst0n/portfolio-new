bf7e208fb80042ea5a5a3210b09d2c5b0671237e
Waldston <waldston.ws@gmail.com>
2024-11-19 19:05:31 -0300

    feat: add font and color theme in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: '#151412',
+        white: '#FDFDFD',
+        gray: '#454346',
+        orange: '#F46C38',
+        green: '#C3FE42',
       },
     },
   },
